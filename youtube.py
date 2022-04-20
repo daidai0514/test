247e02b27d578aa196c0b5aa352b8a047bec0580
daidai0514 <96938300+daidai0514@users.noreply.github.com>
2022-04-20 17:47:11 +0800

    0420

--- youtube.py
+++ youtube.py
@@ -1,11 +1,16 @@
 # Original Problem：https://stackoverflow.com/questions/68945080/pytube-exceptions-regexmatcherror-get-throttling-function-name-could-not-find
 from pytube import YouTube
-import os
+import os,re
+
+ # 去掉非法字元
+def setFileTitle(title):      
+    return re.sub('[\/:*?"<>|]','-',title)
 
 yt = YouTube(input("輸入Youtube影片網址："))
 print(f"開始下載{yt.title}")
 
 path = os.getcwd()+"\\music\\"
+
 # OSError: [Errno 22] Invalid argument：此錯誤為youtube片名有其他符號所導致，與output_path無關
-yt.streams.get_audio_only().download(output_path=path)
+yt.streams.get_audio_only().download(filename=setFileTitle(yt.title)+".mp3", output_path=path)
 print("下載成功")
